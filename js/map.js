92ae0d3dc2b89365ad029d4c3be6b01b76d61a79
olegz7 <olegzubkov09@gmail.com>
2021-04-26 15:39:14 +0000

    updated map.js

--- js/map.js
+++ js/map.js
@@ -1,15 +1,11 @@
-// Initialize and add the map
 function initMap() {
-    // The location of Uluru
-    const uluru = { lat: 40.740, lng: -74.174 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: uluru,
-    });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-      position: uluru,
-      map: map,
-    });
-  }
+  const newark = { lat: 40.740, lng: -74.174 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 10,
+    center: newark,
+  });
+  const marker = new google.maps.Marker({
+    position: newark,
+    map: map,
+  });
+}
